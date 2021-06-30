cb7dc810cf4b41b28f2d550c7c205bb074cd4beb
AlyxMoon <allisterkmoon@gmail.com>
2021-06-30 14:58:58 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Epicodus | Week 06 | API test, getting math facts
+
+## Description
+
+We are to test using an API, I picked math facts
+
+- **Author**: Allister Moon Kays
+- **Live Website Link**: [https://alyxmoon.github.io/epicodus-week06-api-math-facts/](https://alyxmoon.github.io/epicodus-week06-api-math-facts/)
+- **Copyright**: MIT License
---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'API Test | Math Facts!',
+      inject: 'body',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/assets'),
+          to: path.resolve(__dirname, 'dist/assets'),
+        },
+        {
+          from: path.resolve(__dirname, 'src/assets/favicon.ico'),
+          to: path.resolve(__dirname, 'dist/favicon.ico'),
+        },
+        {
+          from: path.resolve(__dirname, 'tests/coverage/lcov-report'),
+          to: path.resolve(__dirname, 'dist/lcov-report'),
+          noErrorOnMissing: true,
+        },
+      ],
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.js/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+        enforce: 'pre',
+      },
+      {
+        test: /\.js/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+
+  devtool: 'eval-source-map',
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+    inline: true,
+  },
+}
